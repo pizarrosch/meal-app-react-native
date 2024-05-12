6e246dedf5856ddd31b41778e9e9ec416a09ccdc
pizarrosch <bayenrshc89@gmail.com>
2024-05-13 00:08:12 +0200

    Adding further styles to the meal item

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,23 @@
-import {View, Text, Pressable, Image, StyleSheet} from "react-native";
+import {View, Text, Pressable, Image, StyleSheet, Platform} from "react-native";
 
 export default function MealItem({title, imageUrl, duration, complexity, affordability}) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Image source={{uri: imageUrl}} style={styles.image}/>
-          <Text style={styles.title}>{title}</Text>
-        </View>
-        <View>
-          <Text>{duration}m</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        style={({pressed}) =>
+         pressed ? {opacity: 0.5} : null }
+        android_ripple={{color: '#ccc'}}
+      >
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{uri: imageUrl}} style={styles.image}/>
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{duration}m</Text>
+            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -19,6 +25,21 @@ export default function MealItem({title, imageUrl, duration, complexity, afforda
 }
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowRadius: 8,
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.35,
+    shadowColor: 'black',
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: 'hidden'
+  },
   image: {
     width: '100%',
     height: 200
@@ -26,6 +47,17 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontFamily: 'roboto-bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    margin: 8
+  },
+  details: {
+    flexDirection: "row",
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  detailItem: {
+    marginHorizontal: 6,
+    fontSize: 12
   }
 })
