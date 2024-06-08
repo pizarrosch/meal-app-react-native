06c461c7d8971f05ec8f724176f9dcc591ad30fa
pizarrosch <bayenrshc89@gmail.com>
2024-06-08 12:43:14 +0200

    Final commit: Add a favorite item to the favorites screen using redux.

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,21 +1,24 @@
-import {useContext} from "react";
-import {FavoritesContext} from "../store/context/favorites-context";
+// import {useContext} from "react";
+// import {FavoritesContext} from "../store/context/favorites-context";
 import {MEALS} from "../dummy-data/Dummy_data";
 import MealsList from "../components/MealsList/MealsList";
 import {StyleSheet, View, Text} from "react-native";
+import {useSelector} from "react-redux";
 
 export default function FavoritesScreen() {
 
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
 
-  const favoriteMeals = MEALS.filter(meal => favoriteMealsCtx.ids.includes(meal.id));
+  const favoriteMealsIds = useSelector((state) => state.favoriteMeals.ids);
+
+  const favoriteMeals = MEALS.filter(meal => favoriteMealsIds.includes(meal.id));
 
   if (favoriteMeals.length === 0) {
     return (
-        <View style={styles.rootContainer}>
-          <Text style={styles.text}>No favorite meals found. Start adding some!</Text>
-        </View>
-      );
+      <View style={styles.rootContainer}>
+        <Text style={styles.text}>No favorite meals found. Start adding some!</Text>
+      </View>
+    );
   }
 
   return (
